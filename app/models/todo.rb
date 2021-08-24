5fd0f55662a251d8da4a0a7dc31d3ed31c1fba30
asyraffff <amirulasyraf4.0x@gmail.com>
2021-08-24 10:13:10 +0800

    Add scope for active and completed todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,20 @@
 class Todo < ApplicationRecord
-end
+    scope :active, -> { where(completed: false) }
+    scope :completed, -> { where(completed: true) }
+  
+    validates :title, presence: true
+  
+    def title=(string)
+      super string.strip
+    end
+  
+    def update_or_destroy(attrs = {})
+      assign_attributes(attrs)
+      title.blank? ? destroy : save
+    end
+  
+    def self.toggle_all!
+      update_all(completed: active.any?)
+    end
+  end
+  
