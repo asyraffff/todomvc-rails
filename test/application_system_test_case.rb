da8d49518ffb158470efd14b2948871918b32671
asyraffff <amirulasyraf4.0x@gmail.com>
2021-08-25 09:47:49 +0800

    System test with selenium

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,10 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |option|
+    option.add_argument('no-sandbox')
+  end
 end
+
+# Remove "Capybara starting Puma..." test logs
+Capybara.server = :puma, { Silent: true }
