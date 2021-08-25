f5164cccb7c755cfafa961ba8a64caca303ae743
asyraffff <amirulasyraf4.0x@gmail.com>
2021-08-25 09:49:05 +0800

    Add Todo model test

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,35 @@
-require "test_helper"
+require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validates presence of title" do
+    todo = Todo.new(title: '')
+    refute todo.valid?
+    assert_includes todo.errors.details[:title], error: :blank
+  end
+
+  test "strips title on assertion" do
+    todo = Todo.new(title: "\t\n Learn Rails \x00\v\f\r ")
+    assert_equal 'Learn Rails', todo.title
+  end
+
+  test "destroy instead of updating when title is blank" do
+    todo = todos(:one)
+    todo.update_or_destroy(title: 'Install Stimulus')
+    refute todo.destroyed?
+
+    todo.update_or_destroy(title: "\t\n")
+    assert todo.destroyed?
+  end
+
+  test "toggle all todos" do
+    assert_difference('Todo.completed.count', 2) do
+      Todo.toggle_all!
+    end
+    assert_difference('Todo.completed.count', -3) do
+      Todo.toggle_all!
+    end
+    assert_difference('Todo.completed.count', 3) do
+      Todo.toggle_all!
+    end
+  end
 end
