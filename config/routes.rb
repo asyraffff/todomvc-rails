863ec00018074b422704c46fff20935b63bbbf40
asyraffff <amirulasyraf4.0x@gmail.com>
2021-08-25 08:29:10 +0800

    Setup routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  root "todos#index"
+  root 'todos#index'
+  get 'active', to: 'todos#index', as: 'active_todos', scope: 'active'
+  get 'completed', to: 'todos#index', as: 'completed_todos', scope: 'completed'
+  resources :todos, except: [:index, :new, :edit] do
+    collection do
+      patch 'toggle_all'
+      delete 'clear_completed'
+    end
+  end
 end
